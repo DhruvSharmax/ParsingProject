c0f39d59f39dc77d2d9273c444715405586de27c
Dhruv <erdhruv2012@gmail.com>
2022-06-30 06:04:58 +0530

    added files

---
+++ src/main/java/com/java/ordersParser/constant/Constants.java
@@ -0,0 +1,7 @@
+package com.java.ordersParser.constant;
+
+public class Constants {
+	public static String JSON = "JSON";
+	public static String CSV = "CSV";
+	public static String result = "Ok";
+}
